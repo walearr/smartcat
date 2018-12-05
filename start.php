61ca8f18635301d182fa5d78112f1e1c39c9a95e
walearr <voldemars.gailitis@gmail.com>
2018-12-05 16:42:24 +0200

    enabled smartcat first question functionality

--- start.php
+++ start.php
@@ -1,17 +1,45 @@
 <?php
+
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    echo str_repeat(PHP_EOL, 50);
-} else {
-    system('reset');
+    $clearScreen = function() {echo str_repeat(PHP_EOL, 50);};
+}
+else {
+    $clearScreen = function() {system('reset');};
 }
 
-include('Cats.php');
-$username = 'User';
+$clearScreen();
 
-echo (new Cats([
+include('Cats.php');
+
+$cat = new Cats();
+echo $cat->catsay([
     'Hello, my name is Smartcat.',
     'Lets play a game.',
-	'What is your name?'
+    'What is your name?'
+])->catRight();
 
-]))->catRight();
-readline($username . ': ');
+$username = readline('My name is: ');
+
+if (strlen($username) > 0) {
+    $clearScreen();
+
+    $number1 = rand(1,20);
+    $number2 = rand(1,20);
+    $result = ($number1 * $number2);
+    $given_answers = array(rand(1,20)*rand(1,20), rand(1,20)*rand(1,20), $result);
+    shuffle($given_answers);
+
+    echo $cat->catsay([
+        'what is multiplication of ' . $number1 . ' and ' . $number2,
+        'answer options are: ' . $given_answers[0] . ' ' .  $given_answers[1] . ' ' . $given_answers[2]
+    ])->cat();
+
+    $answer = readline('My answer to 1st question is: ');
+
+    echo ($answer == $result) ? "correct" : "incorrect";
+}
+else {
+    $clearScreen();
+
+    echo $cat->catsay(['No username given'])->cat();
+}
