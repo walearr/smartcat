19adf363c0a4bada74d06c979372a3809f450abf
walearr <voldemars.gailitis@gmail.com>
2018-12-12 14:53:28 +0200

    addet time constrain for giving answer; minor code refactoring

--- start.php
+++ start.php
@@ -1,4 +1,9 @@
 <?php
+define ("MAX_RESPOND_TIME" ,10);
+
+function randNumber () {
+    return rand(1,10);
+}
 
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     $clearScreen = function() {echo str_repeat(PHP_EOL, 50);};
@@ -22,21 +27,26 @@ $username = readline('My name is: ');
 
 if (strlen($username) > 0) {
     $clearScreen();
-
-    $number1 = rand(1,20);
-    $number2 = rand(1,20);
+    $start_time = time();
+    $number1 = randNumber();
+    $number2 = randNumber();
     $result = ($number1 * $number2);
-    $given_answers = array(rand(1,20)*rand(1,20), rand(1,20)*rand(1,20), $result);
-    shuffle($given_answers);
+    $given_options = array(randNumber() * randNumber(), randNumber() * randNumber(), $result);
+    shuffle($given_options);
 
     echo $cat->catsay([
         'what is multiplication of ' . $number1 . ' and ' . $number2,
-        'answer options are: ' . $given_answers[0] . ' ' .  $given_answers[1] . ' ' . $given_answers[2]
+        'answer options are: ' . $given_options[0] . ' ' .  $given_options[1] . ' ' . $given_answers[2]
     ])->cat();
 
     $answer = readline('My answer to 1st question is: ');
 
-    echo ($answer == $result) ? "correct" : "incorrect";
+    if (MAX_RESPOND_TIME >= (time() - $start_time) AND $answer == $result) {
+        echo "pareizi un laika";
+    }
+    else {
+        echo "nepareiza atbilde/nepaspeji";
+    }
 }
 else {
     $clearScreen();
